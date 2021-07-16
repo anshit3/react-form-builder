c88654f7a7ed82a08e27c525ad2d87f7e551833a
anshit3 <dranrox@gmail.com>
2021-07-16 19:48:54 +0530

    1) Add json view in the thankyou page. 2) Add buttons to navigate back or exit.

--- src/Components/ThankYou/ThankYou.js
+++ src/Components/ThankYou/ThankYou.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Typography, Paper } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+import { Typography, Paper, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+
+import ReactJson from 'react-json-view';
 
 const useStyles = makeStyles({
   root: {
@@ -9,20 +13,66 @@ const useStyles = makeStyles({
       marginTop: 10,
       marginRight: 20,
       marginLeft: 20,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
     },
+  },
+  thankyou_text: {
+    textAlign: 'center',
+  },
+  json_viewer: {
+    height: '330px',
+    overflowY: 'scroll',
+    margin: '15px 60px',
+    border: '#EBEBEB 1px solid',
+    padding: '20px',
+  },
+  exit_button: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  btn_ctn: {
+    width: '200px',
   },
 });
 
 const ThankYou = () => {
   const classes = useStyles();
 
+  const pageConfig = JSON.parse(localStorage.getItem('pageConfig'));
+
   return (
     <div className={classes.root}>
       <Paper elevation={3}>
-        <Typography variant="h3">Thank you for visiting</Typography>
+        <Typography className={classes.thankyou_text} variant="h4">
+          Thank you for visiting. Please check your page structure.
+        </Typography>
+        <div className={classes.json_viewer}>
+          <ReactJson src={pageConfig} />
+        </div>
+        <div className={classes.exit_button}>
+          <div className={classes.btn_ctn}>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to={'/configure'}
+            >
+              Go Back
+            </Button>
+          </div>
+          <div className={classes.btn_ctn}>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to={'/'}
+            >
+              Exit
+            </Button>
+          </div>
+        </div>
       </Paper>
     </div>
   );
